869061ec6b287f7ef12c7841356696fc9f2954a3
Ronjini <ranjinioffice@gmail.com>
2019-12-01 01:45:09 +0530

    users section working 100percent - except security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
+
 use App\Http\Requests;
 
 use App\Photo;
@@ -60,7 +62,7 @@ class AdminUsersController extends Controller
         $input['password'] = bcrypt($request->password);
        User::create($input);
 
-       //return redirect('/admin/users');
+       return redirect('/admin/users');
     }
 
     /**
@@ -84,7 +86,9 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
-         return view('admin.users.edit');
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name','id')->all();
+         return view('admin.users.edit',compact('user','roles'));
     }
 
     /**
@@ -94,9 +98,33 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+
+        if(trim($request->password='')){
+            $input = $request->except('password');
+        } else {
+           $input = $request->all();
+           $input['password'] = bcrypt($request->password); 
+        }
+
+        
+
+        if($file = $request->file('photo_id'))
+        {
+            $name = time().$file->getClientOriginalName();
+            $file->move('images',$name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->update($input);
+        return redirect('/admin/users');
+       // return $request->all();
     }
 
     /**
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,1 +1,47 @@
-edit
+@extends('layouts.admin')
+
+
+@section('content')
+	<h1>Edit Users</h1>
+
+{!! Form::model($user, ['method'=>'PATCH' , 'action'=>['AdminUsersController@update',$user->id],'files' => true]) !!}
+<div class="row">
+	<div class="col-sm-3">
+		<img src="{{$user->photo?$user->photo->file:'http://placehold.it/400x400'}}" alt="" class="img-responsive img-rounded">
+	</div>
+	<div class="form-group">
+		{!! Form::label('name','Title:') !!}
+		{!! Form::text('name', null, ['class' => 'form-control']) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('email','Email:') !!}
+		{!! Form::text('email', null, ['class' => 'form-control']) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('role_id','Role:') !!}
+		{!! Form::select('role_id',$roles, null, ['class' => 'form-control']) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('is_active','Status:') !!}
+		{!! Form::select('is_active', array(1=> 'Active', 0=> 'Not Active'), null, ['class' => 'form-control']) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('photo_id','Photo:') !!}
+		{!! Form::file('photo_id', null, ['class' => 'form-control']) !!}
+	</div>
+
+	<div class="form-group">
+		{!! Form::label('password','Password:') !!}
+		{!! Form::password('password', null, ['class' => 'form-control']) !!}
+	</div>
+	
+	<div class="form-group">
+		{!! Form::submit('Create Post',['class' => 'btn btn-primary']) !!}
+	</div>
+{!! Form::close() !!}
+</div>
+<div class="row">
+	@include('includes.form_errors')
+</div>
+
+@stop
